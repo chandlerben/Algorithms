9ff0c84fcd662bd2d3188fdf8d2df0f8a144c2e0
benjchand <benchand@gmail.com>
2019-07-17 15:42:53 -0700

    Stocks and Recipes are completed. Boom.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,25 @@
+
 #!/usr/bin/python
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    new_batches = float("inf")
+    for item in recipe.keys():
+        if ingredients.__contains__(item) is not True:
+            new_batches = 0
+            break
+        if(ingredients[item]//recipe[item] < new_batches):
+            new_batches = ingredients[item]//recipe[item]
+    batches = new_batches
+    return batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
